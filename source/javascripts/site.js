a1f6b0bdffdc2c68c888a61210a000988a5941fd
Phyu Phyu Hlaing <phyuphyu.h27@gmail.com>
2025-01-14 03:57:16 +0700

    about section

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -6,15 +6,18 @@ document.addEventListener('DOMContentLoaded', () => {
   const span3 = document.getElementById('span3');
   const bannerText = document.querySelector('.banner-text');
 
+  const scrollSection = document.querySelector(".scroll-section");
+  const scrollingDiv = document.querySelector("#about");
+
   window.addEventListener('scroll', () => {
     const scrollY = window.scrollY;
 
-    if (scrollY > 0 && scrollY < 150) {
+    if (scrollY > 0 && scrollY < 100) {
       span1.classList.add('active');
       span2.classList.remove('active');
       span3.classList.remove('active');
       bannerText.classList.remove('active');
-    } else if (scrollY >= 150 && scrollY < 250) {
+    } else if (scrollY >= 100 && scrollY < 250) {
       span1.classList.remove('active');
       span2.classList.add('active');
       span3.classList.remove('active');
@@ -30,5 +33,14 @@ document.addEventListener('DOMContentLoaded', () => {
       // span3.classList.remove('active');
       bannerText.classList.add('active');
     }
+
+    if (scrollSection && scrollingDiv) {
+      const { top, height } = scrollSection.getBoundingClientRect();
+      const scrollPercentage = Math.min(Math.max(-top / height, 0), 1);
+
+      const positionX = (1 - scrollPercentage * 2) * 100;
+      scrollingDiv.style.left = `${positionX}%`;
+    }
   });
+
 });
