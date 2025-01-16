78c1ea6d120130d7b7bfedc2ab0311957d469a67
Phyu Phyu Hlaing <phyuphyu.h27@gmail.com>
2025-01-16 16:51:11 +0700

    changed span js

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,9 +1,7 @@
 // This is where it all goes :)
 
 document.addEventListener('DOMContentLoaded', () => {
-  const span1 = document.getElementById('span1');
-  const span2 = document.getElementById('span2');
-  const span3 = document.getElementById('span3');
+  const spans = Array.from(document.querySelectorAll('.span-text'));
   const bannerText = document.querySelector('.banner-text');
   const projects = document.getElementById('projects');
 
@@ -12,33 +10,34 @@ document.addEventListener('DOMContentLoaded', () => {
 
   window.addEventListener('scroll', () => {
     const scrollY = window.scrollY;
+    const spanPositions = spans.map((span, index) => {
+      const adjustedStart = index === 0 ? span.offsetTop - 300 : span.offsetTop;
+      const adjustedEnd = index === 0 ? adjustedStart + 400 : span.offsetTop + 150;
+      return { start: adjustedStart, end: adjustedEnd };
+    });
     const projectsPosition = projects.offsetTop;
 
-    if (scrollY > 0 && scrollY < 100) {
-      span1.classList.add('active');
-      span2.classList.remove('active');
-      span3.classList.remove('active');
-      bannerText.classList.remove('active');
-    } else if (scrollY >= 100 && scrollY < 250) {
-      span1.classList.remove('active');
-      span2.classList.add('active');
-      span3.classList.remove('active');
-      bannerText.classList.remove('active');
-    } else if (scrollY >= 250 && scrollY < 350) {
-      span1.classList.remove('active');
-      span2.classList.remove('active');
-      span3.classList.add('active');
-      bannerText.classList.remove('active');
-    } else if (scrollY >= 350 && scrollY < projectsPosition) {
-      // span1.classList.remove('active');
-      // span2.classList.remove('active');
-      span3.classList.remove('active');
+
+    spans.forEach((span, index) => {
+      const { start, end } = spanPositions[index];
+
+      if (scrollY >= start && scrollY < end) {
+        span.classList.add('active');
+      } else {
+        span.classList.remove('active');
+      }
+    });
+
+    const lastSpan = spans.length - 1;
+
+    if (scrollY >= spanPositions[lastSpan].end - 50 && scrollY < projectsPosition) {
       bannerText.classList.add('active');
       bannerText.classList.remove('after');
-    } else if (scrollY >= projectsPosition) {
+    } else if ((scrollY >= projectsPosition)) {
       bannerText.classList.remove('active');
       bannerText.classList.add('after');
     } else {
+      bannerText.classList.remove('active');
       bannerText.classList.remove('after');
     }
 
